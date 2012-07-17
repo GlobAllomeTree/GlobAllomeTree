53575ff93bcb3258a20a4208ad4a78b26c4d199a
Thomas Gruner <tom.gruner@gmail.com>
2012-07-17 17:23:17 +0200

    configuration for elasticsearch

--- settings.py
+++ settings.py
@@ -77,17 +77,17 @@ STATIC_ROOT = BASE_PATH + "/static_collected"
 STATIC_URL  = '/static/'
 
 HAYSTACK_CONNECTIONS = {
-#    'default': {
-#        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-#        'URL': 'http://127.0.0.1:9200/',
-#        'INDEX_NAME': 'globallometree',
-#    },
     'default': {
-        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8983/solr'
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'globallometree',
+    },
+#    'default': {
+#        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+#        'URL': 'http://127.0.0.1:8983/solr'
         # ...or for multicore...
         # 'URL': 'http://127.0.0.1:8983/solr/mysite',
-    },
+#    },
 }
 
 try:
