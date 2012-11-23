55fbd9cefc985992b4bab15170a6349cd56d8e49
Marco Santamaria <marco.santamaria@gmail.com>
2012-11-23 17:48:34 +0100

    Changed deployment user.

--- settings_deployment.py
+++ settings_deployment.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'globallometreedb',                      # Or path to database file if using sqlite3.
-        'USER': 'globallometree',                      # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
