93d934e6355d59c55fc09dd2dc68e982c8f87dcc
Thomas Gruner <tom.gruner@gmail.com>
2014-02-26 15:46:20 -0800

    adding in server/startup_bash.sh

---
+++ server/startup_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python /home/docker/code/server/save_env.py
+
+LOCAL_SETTINGS_FILE=/home/docker/code/globallometree/settings_local.py
+
+if [ ! -f $LOCAL_SETTINGS_FILE ];
+then
+   echo "Copying settings_local.py.server to settings_local.py"
+   cp /home/docker/code/globallometree/settings_local.py.server /home/docker/code/globallometree/settings_local.py
+fi
+
+/bin/bash
