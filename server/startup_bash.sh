35508d83551a71044ea9ea6b993ba0919184c9ad
Thomas Gruner <tom.gruner@gmail.com>
2014-02-28 19:37:45 -0800

    added some help text

--- server/startup_bash.sh
+++ server/startup_bash.sh
@@ -10,4 +10,26 @@ then
 fi
 
 cd /home/docker/code
+
+#alias djm=/home/docker/code/manage.py
+#alias go=/home/docker/code/manage.py runserver 0.0.0.0:8083
+
+echo ""
+echo ""
+echo "Welcome to your friendly django debug server, here are a few commands to try out:"
+echo ""
+echo "Run Debug Server "
+echo "./manage.py runserver 0.0.0.0:8083" 
+#echo "djm runserver 0.0.0.0:8083" 
+#echo "go" 
+echo " - then open your browser to http://127.0.0.1:8083/"
+echo ""
+echo "Rebuild Elasticsearch Index"
+echo "./manage.py rebuild_index" 
+echo ""
+
 /bin/bash
+
+echo ""
+echo "Nice to see you and have a great day!!"
+echo ""
