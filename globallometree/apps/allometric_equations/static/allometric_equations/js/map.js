85713c704c5749378a29ed84fc3630db104228d6
Jacob Lesser <jacob@jacoblesser.com>
2014-05-01 16:34:47 +0000

    Map now queries ES on zoomend adjusting the bounding box filter by the maps extent and adjusting the precision of the geohash based on the zoom level

--- globallometree/apps/allometric_equations/static/allometric_equations/js/map.js
+++ globallometree/apps/allometric_equations/static/allometric_equations/js/map.js
@@ -2,6 +2,98 @@ function getURLParameter(name) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
 
+function generateESQuery(q,termFilters,boundingBox,precision) {
+	var query = ejs.Request();
+	var filters;
+	
+	if (boundingBox) {
+		
+		if (boundingBox[0] < -179.99999) {
+			boundingBox[0] = -179.99999;
+		}
+		if (boundingBox[1] < -89.99999) {
+			boundingBox[1] = -89.99999;
+		}
+		if (boundingBox[2] > 179.99999) {
+			boundingBox[2] = 179.99999;
+		}
+		if (boundingBox[3] > 89.99999) {
+			boundingBox[3] = 89.99999;
+		}
+		
+	}
+		
+	if (termFilters && boundingBox) {
+		var geoFilter = ejs.GeoBboxFilter('Locations')
+			.topLeft(ejs.GeoPoint([boundingBox[3], boundingBox[0]]))
+			.bottomRight(ejs.GeoPoint([boundingBox[1], boundingBox[2]]))
+		filters = $.extend([], termFilters, geoFilter);
+	} else {
+		filters = termFilters;
+	}
+	
+
+	if (q) {
+		query.query(
+			ejs.FilteredQuery(
+				ejs.QueryStringQuery({
+					query : q, 
+					fields: ["Keywords"],
+					defaultOperator : "AND"})
+			)
+		)	
+	} else {
+		query.query(
+			ejs.FilteredQuery(
+				ejs.MatchAllQuery(),
+				ejs.AndFilter(filters)
+				
+			)
+		)
+	}
+
+	query.aggregation(
+		ejs.GeoHashGridAggregation('Locations-Grid')
+			.field('Locations')
+			.precision(precision)
+			.aggregation(
+				ejs.TermsAggregation('species')
+					.field('Species')
+			)
+			.aggregation(
+				ejs.AvgAggregation('avg_lat')
+					.script("doc['Locations'].value.lat")
+			)
+			.aggregation(
+				ejs.AvgAggregation('avg_lon')
+					.script("doc['Locations'].value.lon")
+			)
+	)
+	.aggregation(
+		ejs.FilterAggregation('Locations-Bounds')
+			.filter(
+				ejs.ExistsFilter('Locations')
+			)
+			.aggregation(
+				ejs.MinAggregation('min_lat')
+					.script("doc['Locations'].value.lat")
+			)
+			.aggregation(
+				ejs.MaxAggregation('max_lat')
+					.script("doc['Locations'].value.lat")
+			)
+			.aggregation(
+				ejs.MinAggregation('min_lon')
+					.script("doc['Locations'].value.lon")
+			)
+			.aggregation(
+				ejs.MaxAggregation('max_lon')
+					.script("doc['Locations'].value.lon")
+			)
+	);
+	return query;
+}
+
 var mapBounds = [
 	// These coordinates will fit the whole globe when accounting for the padding of 50
 	[-40,-130], //Southwest
@@ -68,8 +160,33 @@ var urlParams = {
 	page : 'ignore'
 }
 
+var geoGridPrecision = {
+	/*zoom level: precision*/
+	0 : 1,
+	1 : 1,
+	2 : 2,
+	3 : 2,
+	4 : 3,
+	5 : 3,
+	6 : 4,
+	7 : 5,
+	8 : 6,
+	9 : 7,
+	10 : 8,
+	11 : 9,
+	12 : 10,
+	13 : 10,
+	14 : 10,
+	15 : 10
+	
+}
+
 //if (document.getElementById('map')) {
-var map = L.map('map');
+var map = L.map('map', {
+	minZoom: 2,
+	maxZoom: 15,
+	noWrap: true
+});
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -78,97 +195,34 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 //Build ES Query from URL Parameters
 
-var andFilter = [];
-/*ejs.GeoBboxFilter('Locations')
-				.topLeft(ejs.GeoPoint([-20.879343,-75.805664]))
-				.bottomRight(ejs.GeoPoint([-56.944974,-52.866211])),*/
+var filters = [];
+var q = getURLParameter('q');
 
 for (param in urlParams) {
 	if (urlParams.hasOwnProperty(param)) {
 		if (urlParams[param] == 'term') {
 			if (getURLParameter(param)) {
-				andFilter.push(ejs.TermFilter(param, getURLParameter(param)))
+				filters.push(ejs.TermFilter(param, getURLParameter(param)))
 			}
 		}
 	}
 }
 
-var query = ejs.Request();
 
-if (getURLParameter('q')) {
-	query.query(
-		ejs.FilteredQuery(
-			ejs.QueryStringQuery({"query" : getURLParameter('q'),"fields": ["Keywords"],"defaultOperator" : "AND"})
-		)
-	)	
-} else {
-	query.query(
-		ejs.FilteredQuery(
-			queryString = ejs.MatchAllQuery(),
-			ejs.AndFilter(andFilter)
-		)
-	)
-}
-
-query.aggregation(
-	ejs.GeoHashGridAggregation('Locations-Grid')
-		.field('Locations')
-		.precision(2)
-		.aggregation(
-			ejs.TermsAggregation('species')
-				.field('Species')
-		)
-		.aggregation(
-			ejs.AvgAggregation('avg_lat')
-				.script("doc['Locations'].value.lat")
-		)
-		.aggregation(
-			ejs.AvgAggregation('avg_lon')
-				.script("doc['Locations'].value.lon")
-		)
-)
-.aggregation(
-	ejs.FilterAggregation('Locations-Bounds')
-		.filter(
-			ejs.ExistsFilter('Locations')
-		)
-		.aggregation(
-			ejs.MinAggregation('min_lat')
-				.script("doc['Locations'].value.lat")
-		)
-		.aggregation(
-			ejs.MaxAggregation('max_lat')
-				.script("doc['Locations'].value.lat")
-		)
-		.aggregation(
-			ejs.MinAggregation('min_lon')
-				.script("doc['Locations'].value.lon")
-		)
-		.aggregation(
-			ejs.MaxAggregation('max_lon')
-				.script("doc['Locations'].value.lon")
-		)
-)
-.toJSON()
-
-$.ajax({
-	type: "POST",
-	url: 'http://localhost:9200/globallometree/allometricequation/_search',
-	data: JSON.stringify(query),
-	success: addToMap
-})
 
 function addToMap (data) {
+	markers = L.featureGroup();
+	
 	var aggIcon = L.divIcon({className: 'agg-icon'});
 	var aggs = data.aggregations['Locations-Grid'].buckets;
-	
+
 	mapBounds = [
 		[data.aggregations['Locations-Bounds']['min_lat'].value, data.aggregations['Locations-Bounds']['min_lon'].value],
 		[data.aggregations['Locations-Bounds']['max_lat'].value, data.aggregations['Locations-Bounds']['max_lon'].value]
 	]
 	
 	for (var i=0;i<aggs.length;i++) {
-		L.marker([
+		var marker = L.marker([
 			aggs[i]['avg_lat'].value,
 			aggs[i]['avg_lon'].value
 		], {
@@ -177,15 +231,40 @@ function addToMap (data) {
 				html: aggs[i]['doc_count'],
 				iconSize: [35, 35]
 			})
-		}).addTo(map);
+		})
+		markers.addLayer(marker);
 	}
+	map.addLayer(markers);
 }
 
 // Set bounds to extent of data.
 $('a[href="#results-map"]').on('shown.bs.tab', function(e){
+	//confirm map zooms on invalidatesize/fitbounds otherwise markers need to be added manually
 	map.invalidateSize();
 	map.fitBounds(mapBounds, {padding: [50, 50]});
-	
 })
+
+var markers = L.featureGroup().addTo(map);
+map.on('zoomend', function() {
+	var bounds = map.getBounds();
+	var maxx = bounds._northEast.lng;
+	var maxy = bounds._northEast.lat;
+	var minx = bounds._southWest.lng;
+	var miny = bounds._southWest.lat;
+	var precision = geoGridPrecision[map.getZoom()];
+	var query = generateESQuery(q, filters, [minx, miny, maxx, maxy], precision);
+	
+	$.ajax({
+		type: "POST",
+		url: 'http://localhost:9200/globallometree/allometricequation/_search',
+		data: JSON.stringify(query.toJSON()),
+		success: function (e) {
+			map.removeLayer(markers)
+			addToMap(e);
+		}
+})
+
+})
+
 //}
 
