7f9b0e493ff120312fb9c9d2676a53d955ded963
Jacob Lesser <jacob@jacoblesser.com>
2014-05-02 15:23:05 +0000

    Cleaned up, made safe, restructured, and commented code

--- globallometree/apps/allometric_equations/static/allometric_equations/js/map.js
+++ globallometree/apps/allometric_equations/static/allometric_equations/js/map.js
@@ -1,103 +1,26 @@
 (function() {
+	//Only use safe Javascript
 	"use strict";
-	function getURLParameter(name) {
-		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
-	}
-
-	function generateESQuery(q,termFilters,boundingBox,precision) {
-		var query = ejs.Request();
-		var filters;
-		
-		if (boundingBox) {
-			
-			if (boundingBox[0] < -179.99999) {
-				boundingBox[0] = -179.99999;
-			}
-			if (boundingBox[1] < -89.99999) {
-				boundingBox[1] = -89.99999;
-			}
-			if (boundingBox[2] > 179.99999) {
-				boundingBox[2] = 179.99999;
-			}
-			if (boundingBox[3] > 89.99999) {
-				boundingBox[3] = 89.99999;
-			}
-			
-		}
-			
-		if (termFilters && boundingBox) {
-			var geoFilter = ejs.GeoBboxFilter('Locations')
-				.topLeft(ejs.GeoPoint([boundingBox[3], boundingBox[0]]))
-				.bottomRight(ejs.GeoPoint([boundingBox[1], boundingBox[2]]));
-			filters = $.extend([], termFilters, geoFilter);
-		} else {
-			filters = termFilters;
-		}
-		
-
-		if (q) {
-			query.query(
-				ejs.FilteredQuery(
-					ejs.QueryStringQuery({
-						query : q, 
-						fields: ["Keywords"],
-						defaultOperator : "AND"})
-				)
-			);	
-		} else {
-			query.query(
-				ejs.FilteredQuery(
-					ejs.MatchAllQuery(),
-					ejs.AndFilter(filters)
-					
-				)
-			);
-		}
-
-		query.aggregation(
-			ejs.GeoHashGridAggregation('Locations-Grid')
-				.field('Locations')
-				.precision(precision)
-				.aggregation(
-					ejs.TermsAggregation('species')
-						.field('Species')
-				)
-				.aggregation(
-					ejs.TermsAggregation('geohashes')
-						.script("doc['Locations'].value.geohash")
-				)
-		)
-		.aggregation(
-			ejs.FilterAggregation('Locations-Bounds')
-				.filter(
-					ejs.ExistsFilter('Locations')
-				)
-				.aggregation(
-					ejs.MinAggregation('min_lat')
-						.script("doc['Locations'].value.lat")
-				)
-				.aggregation(
-					ejs.MaxAggregation('max_lat')
-						.script("doc['Locations'].value.lat")
-				)
-				.aggregation(
-					ejs.MinAggregation('min_lon')
-						.script("doc['Locations'].value.lon")
-				)
-				.aggregation(
-					ejs.MaxAggregation('max_lon')
-						.script("doc['Locations'].value.lon")
-				)
-		);
-		return query;
-	}
-
+	
+	//Prevents older browsers from breaking if a console.log() function is left in the code.
+	if(!window.console){ window.console = {log: function(){} }; } 
+	
+	var filters = [];
+	var q = getURLParameter('q');
+	var param;
+	var markers;
+	var dragTimer;
+	var ES_THROTTLE_RATE = 500; //In ms
+	
+	// These are the default map bounds for the world
+	// coordinates will fit the whole globe when accounting for the padding of 50
 	var mapBounds = [
-		// These coordinates will fit the whole globe when accounting for the padding of 50
 		[-40,-130], //Southwest
 		[40,130] //Northeast
 	];
-
+	
+	//List of all possible GET parameters and the type of value.  May be useful for other
+	//functions other than generating term filters
 	var urlParams = {
 		//Keyword
 		q : 'query',
@@ -157,11 +80,15 @@
 		order_by : 'ignore',
 		page : 'ignore'
 	};
-
+	
+	//Lookup object for determining the Elastic Search GeoHash precision to use
+	//at each map zoom level
+	//Zoom level references can be found at http://wiki.openstreetmap.org/wiki/Zoom_levels
+	//Geohash precision levels can be found at http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/search-aggregations-bucket-geohashgrid-aggregation.html
 	var geoGridPrecision = {
-		/*zoom level: precision*/
-		0 : 1,
-		1 : 1,
+		// zoom level : precision
+		0 : 1, //156,412m/pixel : 5,009.4km x 4,992.6km
+		1 : 1, 
 		2 : 2,
 		3 : 2,
 		4 : 3,
@@ -171,155 +98,227 @@
 		8 : 6,
 		9 : 7,
 		10 : 8,
-		11 : 9,
-		12 : 10,
-		13 : 10,
-		14 : 10,
-		15 : 10
+		11 : 8,
+		12 : 8,
+		13 : 8,
+		14 : 8,
+		15 : 8 //4.773m/pixel : 38.2m x 19m
 	};
-
-	//if (document.getElementById('map')) {
+	
+	//The map object
 	var map = L.map('map', {
 		minZoom: 2,
 		maxZoom: 15,
 		noWrap: true
 	});
-
-	// add an OpenStreetMap tile layer
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
-
-	//Build ES Query from URL Parameters
-
-	var filters = [];
-	var q = getURLParameter('q');
-
-	for (var param in urlParams) {
-		if (urlParams.hasOwnProperty(param)) {
-			if (urlParams[param] == 'term') {
-				if (getURLParameter(param)) {
-					filters.push(ejs.TermFilter(param, getURLParameter(param)));
-				}
-			}
-		}
-	}
-
+		
 	//Modified from:
 	//http://jsfiddle.net/sowelie/3JbNY/
+	//Modifies the Leaflet Marker to have a native hover window
+	//Ensures that hover continues even when moving mouse over symbol text
 	var CustomMarker = L.Marker.extend({
 		bindPopup: function(htmlContent, options) {
 			if (options && options.showOnMouseOver) {
 				//call the super method
 				L.Marker.prototype.bindPopup.apply(this, [htmlContent, options]);
-				
 				//Default behavior shows popup on click
 				//Turn that off so that we only popup on hover.
 				this.off("click", this.openPopup, this);
-				
 				// bind to mouse over
 				this.on("mouseover", function(e) {
-					
 					// get the element that the mouse hovered onto
 					var target = e.originalEvent.fromElement || e.originalEvent.relatedTarget;
 					var parent = this._getParent(target, "leaflet-popup");
-
 					// check to see if the element is a popup, and if it is this marker's popup
 					if (parent == this._popup._container)
 						return true;
-					
 					// show the popup
 					this.openPopup();
-					
 				}, this);
 				
 				// and mouse out
 				this.on("mouseout", function(e) {
-					
 					// get the element that the mouse hovered onto
 					var target = e.originalEvent.toElement || e.originalEvent.relatedTarget;
-					
 					// check to see if the element is a popup
 					if (this._getParent(target, "leaflet-popup")) {
-
 						L.DomEvent.on(this._popup._container, "mouseout", this._popupMouseOut, this);
 						return true;
-
 					}
-					
 					// hide the popup
 					this.closePopup();
-					
 				}, this);
 			}
 		},
 		
 		_popupMouseOut: function(e) {
-			
 			// detach the event
 			L.DomEvent.off(this._popup, "mouseout", this._popupMouseOut, this);
-	 
 			// get the element that the mouse hovered onto
 			var target = e.toElement || e.relatedTarget;
-			
 			// check to see if the element is a popup
 			if (this._getParent(target, "leaflet-popup"))
 				return true;
-			
 			// check to see if the marker was hovered back onto
 			if (target == this._icon)
 				return true;
-			
 			// hide the popup
 			this.closePopup();
-			
 		},
 		
 		_getParent: function(element, className) {
-			
 			var parent = element.parentNode;
-			
 			while (parent !== null) {
-				
 				if (parent.className && L.DomUtil.hasClass(parent, className))
 					return parent;
-				
 				parent = parent.parentNode;
-				
 			}
-			
 			return false;
-			
 		}
-	 
 	});
+	
+	/**
+	* Parses parameters from the URL, spliting by "?" or "&"
+	* @param {string} Name of parameter
+	*/
+	function getURLParameter(name) {
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+	}
 
+	/**
+	* Generates an ElasticS earch aggregation query using the elastic.js library
+	* @param {string} query to be passed to an ES String_Query.  Can be a null value
+	* @param {array} An array of ejs.TermFilter objects. Can be a null value
+	* @param {array} An array of coordinates to filter the results by.  In the order of [minx, miny, maxx, maxy]
+	* @param {integer} A number specifiying the geoHash precision.  Values between 1-12 are acceptable
+	* @return {json}
+	*/
+	function generateESQuery(q,termFilters,boundingBox,precision) {
+		var query = ejs.Request();
+		var filters;
+		
+		if (boundingBox) {
+			//Elastic Search treats values of 180 and -180 as a width of 0 and 90, -90 as a height of 0
+			//Makes sure to pass values that will actually return the required results.
+			//Additionally, Leaflet will pan from world copy to world copy.  This can create values infinitly
+			//far outside of allowed geographic coordinates.  This snaps requests back to the primary map
+			//We can prevent leaflet from loading outside of the defined geographic coordinates for the globe
+			if (boundingBox[0] < -179.99999) {
+				boundingBox[0] = -179.99999;
+			}
+			if (boundingBox[1] < -89.99999) {
+				boundingBox[1] = -89.99999;
+			}
+			if (boundingBox[2] > 179.99999) {
+				boundingBox[2] = 179.99999;
+			}
+			if (boundingBox[3] > 89.99999) {
+				boundingBox[3] = 89.99999;
+			}
+		}
+		
+		//Builds the term filters for the ES search concatenating the term filters and bounding
+		//box filters if both are set
+		if (termFilters && boundingBox) {
+			var geoFilter = ejs.GeoBboxFilter('Locations')
+				.topLeft(ejs.GeoPoint([boundingBox[3], boundingBox[0]]))
+				.bottomRight(ejs.GeoPoint([boundingBox[1], boundingBox[2]]));
+			filters = $.extend([], termFilters, geoFilter);
+		} else {
+			filters = termFilters;
+		}
+		
+		//Creaates a simple_string query if the q parameter is set
+		//Defaults to only searching the "keywords" field using an "AND" operator
+		//If the q parameter is set, term filters are ignored since ES won't accept both
+		if (q) {
+			query.query(
+				ejs.FilteredQuery(
+					ejs.QueryStringQuery({
+						query : q, 
+						fields: ["Keywords"],
+						defaultOperator : "AND"})
+				)
+			);
+		//Otherwise we just set the query to MatchAll and filter by term filters
+		} else {
+			query.query(
+				ejs.FilteredQuery(
+					ejs.MatchAllQuery(),
+					ejs.AndFilter(filters)
+					
+				)
+			);
+		}
 
+		//Build the aggreagations
+		query.aggregation(
+			ejs.GeoHashGridAggregation('Locations-Grid')
+				.field('Locations')
+				.precision(precision)
+				.aggregation(
+					ejs.TermsAggregation('species')
+						.field('Species')
+				)
+				.aggregation(
+					ejs.TermsAggregation('geohashes')
+						.script("doc['Locations'].value.geohash")
+				)
+		)
+		.aggregation(
+			ejs.FilterAggregation('Locations-Bounds')
+				.filter(
+					ejs.ExistsFilter('Locations')
+				)
+				.aggregation(
+					ejs.MinAggregation('min_lat')
+						.script("doc['Locations'].value.lat")
+				)
+				.aggregation(
+					ejs.MaxAggregation('max_lat')
+						.script("doc['Locations'].value.lat")
+				)
+				.aggregation(
+					ejs.MinAggregation('min_lon')
+						.script("doc['Locations'].value.lon")
+				)
+				.aggregation(
+					ejs.MaxAggregation('max_lon')
+						.script("doc['Locations'].value.lon")
+				)
+		);
+		return query;
+	}
+
+	/**
+	* Adds allometric aggregations to the map
+	* @param {json} return object from an Elastic Search query 
+	*/
 	function addToMap (data) {
 		markers = L.featureGroup();
-		
-		var aggIcon = L.divIcon({className: 'agg-icon'});
+		var i;
 		var aggs = data.aggregations['Locations-Grid'].buckets;
 		
-
+		//Updates the map bounds object that contains all of the request points
 		mapBounds = [
 			[data.aggregations['Locations-Bounds']['min_lat'].value, data.aggregations['Locations-Bounds']['min_lon'].value],
 			[data.aggregations['Locations-Bounds']['max_lat'].value, data.aggregations['Locations-Bounds']['max_lon'].value]
 		];
 		
-		for (var i=0;i<aggs.length;i++) {
-			
+		for (i=0;i<aggs.length;i++) {
 			var html = "";
 			var j;
+			var totalLat = 0;
+			var totalLon = 0;
+			var totalLocations = 0;
+			var iconSize;
+			var sizeClass;
+			
+			//Generate text for html hover events
 			for (j=0;j<aggs[i].species.buckets.length;j++){
 				html += aggs[i].species.buckets[j].key + " (" + aggs[i].species.buckets[j]['doc_count']  + ")<br>";
 			}
 			
-			var totalDocs = 0;
-			var totalLat = 0;
-			var totalLon = 0;
-			var totalLocations = 0;
-
 			//Here we loop through and get locations that are inside the geohash 
 			//we are looking for. (since documents have many locations and only some
 			//of them are inside the current geohash, this is required)
@@ -336,8 +335,8 @@
 				}  
 			}
 			
-			var iconSize = aggs[i]['doc_count'] / 15 + 10;
-			var sizeClass;
+			//Set the marker icon size based on the number of aggregations
+			iconSize = aggs[i]['doc_count'] / 15 + 10;
 			if(iconSize > 35) iconSize = 35;
 			if (iconSize > 10 && iconSize < 11 ) {
 				iconSize = 14;
@@ -380,14 +379,14 @@
 		}
 		map.addLayer(markers);
 	}
-
-	// Set bounds to extent of data.
-	$('a[href="#results-map"]').on('shown.bs.tab', function(e){
-		//confirm map zooms on invalidate size/fitbounds otherwise markers need to be added manually
-		map.invalidateSize();
-		map.fitBounds(mapBounds, {padding: [50, 50]});
-	});
-
+	
+	/**
+	* Generates and elastic search query and retrieves the results from the server
+	* drawing them on the map.  Query is limited to the extent of the map view (plus buffer)
+	* to limit the number of features returned from the server and drawn on the map.  This
+	* is particularly important when zoomed in as the number of features could be prohibitively
+	* large
+	*/
 	function updateMarkers() {
 		var bounds = map.getBounds();
 		var maxx = bounds._northEast.lng;
@@ -407,17 +406,45 @@
 			}
 		});
 	}
+	
+	//Leaflet requires the div to be of a defined size before generating the map
+	//When leaflet is called on a hidden div, the map is created with a size of 0
+	//When we show the map, we need to reset the map so it draws at the correct dimensions
+	//Hiding and reshowing the map tab will reset the map to the mapBounds.  If the map
+	//should be reset to the last zoom and extent the user was viewing, then we need to 
+	//update the map bounds with an "onmove" event listener
+	$('a[href="#results-map"]').on('shown.bs.tab', function(){
+		//confirm map zooms on invalidate size/fitbounds otherwise markers need to be added manually
+		map.invalidateSize();
+		map.fitBounds(mapBounds, {padding: [50, 50]});
+	});
+	
+	// add an OpenStreetMap tile layer
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
 
-	var markers = L.featureGroup().addTo(map);
+	//Build ES Query from URL Parameters
+	for (param in urlParams) {
+		if (urlParams.hasOwnProperty(param)) {
+			if (urlParams[param] == 'term') {
+				if (getURLParameter(param)) {
+					filters.push(ejs.TermFilter(param, getURLParameter(param)));
+				}
+			}
+		}
+	}
+
+	markers = L.featureGroup().addTo(map);
 	map.on('zoomend', updateMarkers);
 
-	var dragTimer = null;
+	dragTimer = null;
 	map.on('drag', function() {
 		if (dragTimer === null) {
 			dragTimer = setTimeout(function(){
 				updateMarkers(); 
 				dragTimer = null;
-			}, 500);
+			}, ES_THROTTLE_RATE);
 		}
 	});
 }());
