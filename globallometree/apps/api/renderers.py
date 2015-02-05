718489c6992fd354e1899f6ae652607b0c8a532b
Thomas Gruner <tom.gruner@gmail.com>
2015-02-04 21:26:24 -0800

    updating the requirements

--- globallometree/apps/api/renderers.py
+++ globallometree/apps/api/renderers.py
@@ -3,33 +3,42 @@ from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.renderers import XMLRenderer
 from rest_framework_csv.renderers import CSVRenderer
-
+from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
 
 class SimpleJSONRenderer(JSONRenderer):
-	format='json'
+    format='json'
 
 
 class SimpleXMLRenderer(XMLRenderer):
-	format='xml'
+    format='xml'
 
 
 class SimpleBrowsableAPIRenderer(BrowsableAPIRenderer):
-	format='api'
+    format='api'
 
 
 class SimpleCSVRenderer(CSVRenderer):
-	format='csv'
+    expand_lists = 'vertical'
+    sort_headers = False
+
+    def get_data(self, data):
+        if type(data) == ReturnDict:
+            data = dict(data)
+            return data['results']
+        if type(data) == ReturnList:
+            data = [data]
+        return data
 
 
 class FullJSONRenderer(JSONRenderer):
-	format='json-full'
+    format='json-full'
 
 
 class FullXMLRenderer(XMLRenderer):
-	format='xml-full'
+    format='xml-full'
 
 
 class FullBrowsableAPIRenderer(BrowsableAPIRenderer):
-	format='api-full'
+    format='api-full'
 
 
