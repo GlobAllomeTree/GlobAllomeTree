b578ba831176723682b5829f94a5cc50fe085a97
Thomas Gruner <tom.gruner@gmail.com>
2015-03-15 20:10:53 -0700

    refactor search app to work for wood density and raw data

--- globallometree/apps/search_helpers/static/search_helpers/js/search.js
+++ globallometree/apps/search_helpers/static/search_helpers/js/search.js
@@ -8,60 +8,28 @@ window.app.searchManager = function (){
 
 	var searchDict;
 
+	var indexName = 'allometricequation';
+
+	var baseTermFilterKeys = [
+			'Genus',
+			'Species',
+			'Country',
+			'Biome_HOLDRIDGE',
+			'Biome_UDVARDY',
+			'Biome_FAO',
+			'Biome_WWF',
+			'Division_BAILEY',
+			'Author',
+			'Reference',
+			'Year'];
+
 	//Simple term filters which can be string or boolean mapping types
 	//When boolean 0,f alse, off, no and and empty string are false 
-	var termFilterKeys = [  
-		'Ecosystem', 
-		'Population',
-		'Genus',
-		'Species',
-		'Country',
-		'Biome_HOLDRIDGE',
-		'Biome_UDVARDY',
-		'Biome_FAO',
-		'Biome_WWF',
-		'Division_BAILEY',
-		'Author',
-		'Reference',
-		'Year',
-		'B',
-		'Bd',
-		'Bg',
-		'Bt',
-		'L',
-		'Rb',
-		'Rf',
-		'Rm',
-		'S',
-		'T',
-		'F',
-		'U',
-		'V',
-		'W',
-		'X',
-		'Z',
-		'Unit_U', 
-		'Unit_V',
-		'Unit_W',
-		'Unit_X',
-		'Unit_Y',
-		'Unit_Z',
-		'Output'
-	];
+	var config = {
+		termFilterKeys : [],
+	    rangeFilterKeys :[]
+	}
 
-	//range filters
-	var rangeFilterKeys = [
-		'Min_X__gte',
-		'Max_X__gte',
-		'Min_Z__gte',
-		'Max_Z__gte',
-		'Min_X__lte',
-		'Max_X__lte',
-		'Min_Z__lte',
-		'Max_Z__lte'
-	]
-
-	
 	var setSearchDict = function(dict) {
 		//Save the search dictionary in the function context
 		searchDict = dict;
@@ -70,7 +38,7 @@ window.app.searchManager = function (){
 	var search = function(params) {
 		$.ajax({
 			type: "POST",
-			url: window.app.config.search_url + '/globallometree/allometricequation/_search',
+			url: window.app.config.search_url + '/globallometree/' + indexName + '/_search',
 			data: JSON.stringify(params['query'].toJSON()),
 			success: params['success']
 		});
@@ -87,6 +55,9 @@ window.app.searchManager = function (){
 	}
 	
 	var getQuery = function (params) {
+
+		var termFilterKeys = _.extend(baseTermFilterKeys, config.termFilterKeys);
+
 		//This base getQuery function is called by both the map and 
 		//list views
 		//The map view sometimes needs to pass a more restrictive bounding box
@@ -128,7 +99,7 @@ window.app.searchManager = function (){
 				filters.push(ejs.TermFilter(key, searchValue));
 			}
 			//Range filters
-			if($.inArray(key, rangeFilterKeys) != -1) {
+			if($.inArray(key, config.rangeFilterKeys) != -1) {
 				//Key is assumed to look like fieldname__gte
 				var parts = key.split('__')
 				var field = parts[0];
@@ -290,7 +261,9 @@ window.app.searchManager = function (){
 		getQuery : getQuery,
 		search : search,
 		getLink : getLink,
-		getCurrentSearchDict : getCurrentSearchDict	
+		getCurrentSearchDict : getCurrentSearchDict,
+		indexName : indexName,
+		config : config
 	}
 
 }();
