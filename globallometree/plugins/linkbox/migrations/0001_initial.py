f69360a4b5d39064599821ee0f7fc3c3616e34a5
Thomas Gruner <tom.gruner@gmail.com>
2015-09-02 15:23:59 -0700

    fixing migration dependencies

--- globallometree/plugins/linkbox/migrations/0001_initial.py
+++ globallometree/plugins/linkbox/migrations/0001_initial.py
@@ -6,6 +6,9 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
+    dependencies = [
+        ('cms', '0012_auto_20150607_2207'),
+    ]
 
     operations = [
         migrations.CreateModel(
