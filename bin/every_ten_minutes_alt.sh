515a7a6f4051e41b0b343b0b4e03dcfdc21048e9
Thomas Gruner <tom.gruner@gmail.com>
2015-09-09 04:36:52 -0700

    specify index to avoid errors stopping other indexes

--- bin/every_ten_minutes_alt.sh
+++ bin/every_ten_minutes_alt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/opt/globallometree_virtualenv/bin/python /opt/globallometree_app/manage.py sync_elasticsearch
+/opt/globallometree_virtualenv/bin/python /opt/globallometree_app/manage.py sync_elasticsearch rawdata
+/opt/globallometree_virtualenv/bin/python /opt/globallometree_app/manage.py sync_elasticsearch wooddensity
+/opt/globallometree_virtualenv/bin/python /opt/globallometree_app/manage.py sync_elasticsearch allometricequation
+/opt/globallometree_virtualenv/bin/python /opt/globallometree_app/manage.py sync_elasticsearch biomassexpansionfactor
