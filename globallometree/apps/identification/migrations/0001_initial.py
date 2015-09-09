80f7831781a05513b18162e916b33d2a0e7a92bd
Thomas Gruner <tom.gruner@gmail.com>
2015-09-09 02:20:17 -0700

    refactor TreeType, Vegetaion Type

---
+++ globallometree/apps/identification/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TreeType',
+            fields=[
+                ('Created', models.DateTimeField(auto_now_add=True, db_column=b'created')),
+                ('Modified', models.DateTimeField(auto_now=True, verbose_name=b'Last modified', db_column=b'modified')),
+                ('ID_Tree_type', models.AutoField(serialize=False, primary_key=True, db_column=b'id_tree_type')),
+                ('Name', models.CharField(max_length=255, null=True, db_column=b'name', blank=True)),
+            ],
+            options={
+                'ordering': ('Name',),
+                'db_table': 'identification_tree_type',
+            },
+        ),
+        migrations.CreateModel(
+            name='VegetationType',
+            fields=[
+                ('Created', models.DateTimeField(auto_now_add=True, db_column=b'created')),
+                ('Modified', models.DateTimeField(auto_now=True, verbose_name=b'Last modified', db_column=b'modified')),
+                ('ID_Vegetation_type', models.AutoField(serialize=False, primary_key=True, db_column=b'id_vegetation_type')),
+                ('Name', models.CharField(max_length=255, null=True, db_column=b'name', blank=True)),
+            ],
+            options={
+                'ordering': ('Name',),
+                'db_table': 'identification_vegetation_type',
+            },
+        ),
+    ]
