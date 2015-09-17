c9518287c9d128dc416b06ee4582f06735658b34
Thomas Gruner <tom.gruner@gmail.com>
2015-09-17 10:45:21 -0700

    bug fix

--- globallometree/apps/base/static/base/js/list.js
+++ globallometree/apps/base/static/base/js/list.js
@@ -225,17 +225,6 @@ window.app.listController = function () {
 				context['datasetLink'] = '';
 			}
 
-			try {
-				context['Density_g_cm3'] = Number(context['Density_g_cm3']).toFixed(3)
-			} catch(e) {
-
-			}
-
-			try {
-				context['BEF'] = Number(context['BEF']).toFixed(3)
-			} catch(e) {
-
-			}
 
 			$resultsList.append(Mustache.render(getResultTemplate(), context));	
 		}
